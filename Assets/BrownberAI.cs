d0b286ed63cd2c34a3c1eccf9d030bc5f6f72a35
Jerry <jjs54@njit.edu>
2017-04-20 03:49:27 -0400

    First commit

---
+++ Assets/BrownberAI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrownberAI : MonoBehaviour {
+    public UnityEngine.AI.NavMeshAgent brownber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Grab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour {
+    bool isgrab;
+    void Grabbing()
+    {
+        isgrab = true;
+    }
+    void Dropping()
+    {
+        isgrab = false;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
